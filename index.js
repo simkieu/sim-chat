60cf818a574d50db78e0d1d70f15108de588e00b
Sim Kieu <simkieu@gmail.com>
2017-08-20 13:39:18 -0700

    test

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var Peer = require('simple-peer'),
-    peer = new Peer({
-        initiator: location.hash === '#init',
-        trickle: false
-    }),
-    express = require('express'),
+// var Peer = require('simple-peer'),
+//     peer = new Peer({
+//         initiator: location.hash === '#init',
+//         trickle: false
+//     }),
+var express = require('express'),
     app = express(),
-    // server = require('http').createServer(app),
-    // io = require('socket.io').listen(server),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
     users = [],
     connections = [];
-peer.on('signal', function(data) {
-    document.getElementById('yourID').value = JSON.stringify(data);
-})
+// peer.on('signal', function(data) {
+//     document.getElementById('yourID').value = JSON.stringify(data);
+// });
 // app.use(express.static('styles'));
 // app.use("/styles", express.static(__dirname + "/styles"));
 console.log('Server running...');
-// server.listen(process.env.PORT || 3000);
+server.listen(9966);
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
