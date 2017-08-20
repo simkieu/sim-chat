3dd7ecdb1d8f6d332f30624101ccb29f6809af90
Sim Kieu <simkieu@gmail.com>
2017-08-20 02:31:55 -0700

    test css

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     users = [],
     connections = [];
-
+app.use(express.static('styles'));
+app.use("/styles", express.static(__dirname + "/styles"));
 console.log('Server running...');
 server.listen(process.env.PORT || 3000);
 app.get('/', function(req, res) {
