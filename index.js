77287220818f4d2ab253791184718421d34b3eae
Sim Kieu <simkieu@gmail.com>
2017-08-20 13:08:06 -0700

    use bundle

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+var Peer = require('simple-peer'),
+    peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false
+    }),
+    // express = require('express'),
+    // app = express(),
+    // server = require('http').createServer(app),
+    // io = require('socket.io').listen(server),
     users = [],
     connections = [];
-app.use(express.static('styles'));
-app.use("/styles", express.static(__dirname + "/styles"));
-console.log('Server running...');
-server.listen(process.env.PORT || 3000);
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+peer.on('signal', function(data) {
+    document.getElementById('yourID').value = JSON.stringify(data);
+})
+// app.use(express.static('styles'));
+// app.use("/styles", express.static(__dirname + "/styles"));
+// console.log('Server running...');
+// server.listen(process.env.PORT || 3000);
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
